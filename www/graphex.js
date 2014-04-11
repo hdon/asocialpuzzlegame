baec9cee000158342851f3b0e72297d3e0111944
Donny Viszneki <don@codebad.com>
2014-04-11 13:29:26 -0700

    some nodes floating around on the screen

--- www/graphex.js
+++ www/graphex.js
@@ -4,11 +4,13 @@ var pingInterval
   , ping
   , pingSeq = 0
   , socket
-  , canvas
-  , draw
+  , graphex
   ;
 
 function init() {
+  var canvas
+    ;
+
   socket = io.connect(SOCKET_IO_HOST);
 
   /* Handle chat messages from server */
@@ -59,8 +61,14 @@ function init() {
     css('top', '0px').
     css('left', '0px').
     appendTo(document.body);
-  draw = canvas.getContext('2d');
 
+  graphex = newgraphex(canvas);
+  graphex.addNode('foo');
+  graphex.addNode('bar');
+  graphex.addNode('baz');
+  graphex.addNode('qux');
+  //graphex.addNode('bar');
+  graphex.updateAndRender();
 }
 
 function newgraph() {
@@ -77,7 +85,7 @@ function newgraph() {
   function addNode (id, data) {
     if (nodes.hasOwnProperty(id))
       throw new Error('Node exists');
-    nodes[node] = {
+    nodes[id] = {
       edges: [],
       data: data
     }
@@ -96,6 +104,8 @@ function newgraph() {
     requireNode(a);
     requireNode(b);
     id = edgeID(a, b);
+    if (edges.hasOwnProperty(id))
+      throw new Error('edge exists');
     if (arguments.length < 3)
       data = {};
     edges[id] = {
@@ -103,6 +113,8 @@ function newgraph() {
     , a:    a
     , b:    b
     };
+    nodes[a].edges.push(id);
+    nodes[b].edges.push(id);
     return data;
   }
 
@@ -111,6 +123,18 @@ function newgraph() {
       id = edgeID(id, arguments[1]);
     requireEdge(id);
     delete edges[id];
+  }
+
+  function getNodeData(id) {
+    requireNode(id);
+    return nodes[id].data;
+  }
+
+  function getEdgeData(id) {
+    if (arguments.length == 2)
+      id = edgeID(id, arguments[1]);
+    requireEdge(id);
+    return edges[id].data;
   }
 
   return {
@@ -123,14 +147,121 @@ function newgraph() {
   , removeNode : removeNode 
   , addEdge: addEdge
   , removeEdge: removeEdge
+  , _getNodes: function(){return nodes}
+  , _getEdges: function(){return edges}
   };
 }
 
-function newgraphex() {
+function newgraphex(canvas) {
   var graph = newgraph()
+    , nodes = graph._getNodes()
+    , edges = graph._getEdges()
+    , draw = canvas.getContext('2d')
+    , viewTranslationX = canvas.width / 2
+    , viewTranslationY = canvas.height / 2
     ;
 
-  
+  function addNode(id) {
+    var data = {
+      name: id
+    , x: 0
+    , y: 0
+    , vx: Math.random()
+    , vy: Math.random()
+    , r: 12
+    };
+    return graph.addNode(id, data);
+  }
+
+  function _calculateDistance(a, b) {
+    return Math.sqrt(_calculateDistanceSquared(a, b));
+  }
+
+  function _calculateDistanceSquared(a, b) {
+    var dx = a.x - b.x
+      , dy = a.y - b.y;
+    return dx*dx + dy*dy;
+  }
+
+  function render() {
+    var x0, y0, x1, y1, nodeID, a, b;
+
+    draw.clearRect(0, 0, canvas.width, canvas.height);
+
+    draw.fillStyle = 'red';
+    for (nodeID in nodes) {
+      a = nodes[nodeID].data;
+      x0 = a.x + viewTranslationX;
+      y0 = a.y + viewTranslationY;
+      draw.fillText(nodeID, x0, y0);
+    }
+  }
+
+  function updateAndRender() {
+    var maxv;
+    maxv = update(); // TODO time!
+    render();
+    if (maxv > 0.1)
+      requestAnimFrame(updateAndRender);
+    else
+      console.log('graphex stable');
+  }
+
+  function update() {
+    var nodeIDs = Object.keys(nodes)
+      , node , id , a , b , d, data
+      , vsq, maxvsq
+      ;
+
+    /* TODO BSP */
+
+    /* Force which keeps nodes from overlapping */
+    for (i = 0; i < nodeIDs.length; i++) {
+      a = nodes[nodeIDs[i]];
+      for (j = i+1; j < nodeIDs.length; j++) {
+        b = nodes[nodeIDs[j]];
+        d = _calculateDistanceSquared(a.data, b.data);
+        /* TODO distance-based effect from a single point is stupid */
+        if (d < a.data.r * a.data.r + b.data.r * b.data.r) {
+          a.data.vy += (a.data.y > b.data.y) ? 1 : -1;
+          a.data.vx += (a.data.x > b.data.x) ? 1 : -1;
+          b.data.vy += -a.data.vy;
+          b.data.vx += -a.data.vx;
+        }
+      }
+    }
+
+    maxvsq = 0;
+    for (id in nodes) {
+      data = nodes[id].data;
+
+      /* Find velocity magnitude squared */
+      vsq = data.vx * data.vx + data.vy * data.vy;
+
+      /* Find max velocity of all nodes */
+      if (maxvsq < vsq)
+        maxvsq = vsq;
+
+      /* Integrate velocity -> position */
+      data.x += data.vx;
+      data.y += data.vy;
+
+      /* Some sort of force to settle things down */
+      data.vx *= 0.7;
+      data.vy *= 0.7;
+    }
+
+    /* Return the max node velocity, as a sort of way of hinting that we might be stable */
+    return Math.sqrt(maxvsq);
+  }
+
+  return {
+    addNode: addNode
+  , updateAndRender: updateAndRender
+  , _getNodes: function(){return nodes}
+  , _getEdges: function(){return edges}
+  , _render: render
+  }
 }
 
 function render() {
