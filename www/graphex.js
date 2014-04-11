c8ea5fb7b14682f1f6be21286264c855d6179e1c
Donny Viszneki <don@codebad.com>
2014-04-11 12:45:35 -0700

    some kinda graph code

--- www/graphex.js
+++ www/graphex.js
@@ -4,6 +4,8 @@ var pingInterval
   , ping
   , pingSeq = 0
   , socket
+  , canvas
+  , draw
   ;
 
 function init() {
@@ -47,6 +49,108 @@ function init() {
     socket.emit('ping', { seq: pingSeq });
     pingSeq++;
   }
+
+  /* Create canvas */
+  canvas = document.createElement('canvas');
+  canvas.width = $(window).width();
+  canvas.height = $(window).height() - 23;
+  $(canvas).
+    css('position', 'absolute').
+    css('top', '0px').
+    css('left', '0px').
+    appendTo(document.body);
+  draw = canvas.getContext('2d');
+
 }
+
+function newgraph() {
+  var nodes = {}
+    , edges = {}
+    ;
+
+  function edgeID(a, b) { return escape(a) + ':' + escape(b) }
+  function nodeExists (id) { return nodes.hasOwnProperty(id) }
+  function requireNode (id) { if (!nodeExists(id)) throw new Error('no such node') }
+  function edgeExists (id) { return edges.hasOwnProperty(id) }
+  function requireEdge (id) { if (!edgeExists(id)) throw new Error('no such edge') }
+
+  function addNode (id, data) {
+    if (nodes.hasOwnProperty(id))
+      throw new Error('Node exists');
+    nodes[node] = {
+      edges: [],
+      data: data
+    }
+  }
+
+  function removeNode (id) {
+    requireNode(id);
+    /* Remove all edges containing node */
+    nodes[id].edges.forEach(removeEdge);
+    /* Remove node itself */
+    delete nodes[node];
+  }
+
+  function addEdge(a, b, data) {
+    var id;
+    requireNode(a);
+    requireNode(b);
+    id = edgeID(a, b);
+    if (arguments.length < 3)
+      data = {};
+    edges[id] = {
+      data: data
+    , a:    a
+    , b:    b
+    };
+    return data;
+  }
+
+  function removeEdge(id) {
+    if (arguments.length == 2)
+      id = edgeID(id, arguments[1]);
+    requireEdge(id);
+    delete edges[id];
+  }
+
+  return {
+    edgeID: edgeID
+  , nodeExists : nodeExists 
+  , requireNode : requireNode 
+  , edgeExists : edgeExists 
+  , requireEdge : requireEdge 
+  , addNode : addNode 
+  , removeNode : removeNode 
+  , addEdge: addEdge
+  , removeEdge: removeEdge
+  };
+}
+
+function newgraphex() {
+  var graph = newgraph()
+    ;
+
+  
+}
+
+function render() {
+  draw.fillStyle = 'black';
+  draw.fillRect(
+    Math.random() * canvas.width,
+    Math.random() * canvas.height,
+    64, 64);
+  // if we need to draw updates...
+  requestAnimFrame(render);
+}
+
+/* http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/ */
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
 $(init);
